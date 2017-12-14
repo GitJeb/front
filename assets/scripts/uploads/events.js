179f24563f6a78bcaf28e735d5bc1e7c70546398
gercks <elizagercke@gmail.com>
2017-12-14 17:00:05 -0500

    Add api actions for uploads
    - create new folder under assets/scripts
    - add events.js, api.js, ui.js and respective code
    - add event handler in index.js

---
+++ assets/scripts/uploads/events.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// const getFormFields = require('../../../lib/get-form-fields')
+
+const uploadApi = require('./api')
+const uploadUi = require('./ui')
+
+const createUploadMultiPart = function (event) {
+  event.preventDefault()
+  console.log('it did something in multipart')
+
+  // let data = getFormFields(event.target)
+  const data = new FormData(event.target)
+
+  uploadApi.createMulti(data)
+    .then(uploadUi.success)
+    .catch(uploadUi.error)
+}
+
+module.exports = {
+  createUploadMultiPart
+}
