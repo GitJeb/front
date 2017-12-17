23b358eafdd2258a5f9f5e2d85306ff357974795
gercks <elizagercke@gmail.com>
2017-12-17 13:56:06 -0500

    Begin using hidden class instead of jquery hide
    still need to fix ui

--- assets/scripts/uploads/events.js
+++ assets/scripts/uploads/events.js
@@ -18,7 +18,6 @@ const createUploadMultiPart = function (event) {
 
 // Show all images uploaded
 const onShowIndex = function () {
-  $('.update-form').hide()
   uploadApi.indexAll()
     .then(uploadUi.indexAllSuccess)
     .catch(uploadUi.indexAllFail)
